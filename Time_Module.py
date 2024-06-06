f9246dbd0e0892f317d5eea5486f826ec79cf304
Pankaj Patil <97191429+Pankajp1997@users.noreply.github.com>
2024-06-06 17:33:55 +0000

    Added the Contents in the time module

--- Time_Module.py
+++ Time_Module.py
@@ -41,11 +41,62 @@ print(td + tdelta)
 
 tdelta = datetime.timedelta(hours=12)
 print(td + tdelta)
-'''
+
 dt_today = datetime.datetime.today()
 dt_now = datetime.datetime.now()
 dt_utcnow = datetime.datetime.utcnow()
 
 print(dt_today)
 print(dt_now)
-print(dt_utcnow)
+print(dt_utcnow) # it is also not a time aware 
+
+# we have explicitly made the timezone 
+# pip install pytz 
+# The standard library has the timezone class for handling the fixed offset and timezone.utc as UTC timezobe instance. 
+import pytz 
+dt = datetime.datetime(2024,7,27,12,30,45,tzinfo=pytz.UTC)
+print(dt)
+
+#dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+#print(dt_utcnow)
+
+dt_now = datetime.datetime.now(tz=pytz.UTC)
+print(dt_now) # it is timezone aware at last + 
+
+dt_mytime = dt_now.astimezone(pytz.timezone('ASIA/kolkata'))
+print(dt_mytime)
+
+
+# to see all the timezones 
+for tz in pytz.all_timezones:
+    print(tz)
+'''
+'''
+import pytz
+dt_now = datetime.datetime.now(tz=pytz.UTC)
+#print(dt_now) # it is timezone aware at last + 
+
+dt_mtn = datetime.datetime.now()
+mtn_tz = pytz.timezone('US/Mountain')
+dt_mtn= mtn_tz.localize(dt_mtn)
+print(dt_mtn)
+
+
+dt_east = dt_mtn.astimezone(pytz.timezone('Asia/kolkata'))
+print(dt_east)
+
+'''
+import pytz 
+
+dt_mtn = datetime.datetime.now(tz=pytz.timezone('Asia/Kolkata'))
+print(dt_mtn.strftime('%B %d,%Y')) # searchg format codes for the dates and print it out
+
+#opposite is if you have datetime and you want to do it into datetime 
+
+dt_str = 'June 14, 2024'
+
+dt = datetime.datetime.strptime(dt_str, '%B %d, %Y')
+print(dt)
+
+# strftime -- datetime to string 
+# strptime -- string to datetime 

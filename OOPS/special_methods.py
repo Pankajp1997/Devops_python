d53edb7e3f88bf40fd91cd56d2c1d2cfe0173279
Pankaj Patil <97191429+Pankajp1997@users.noreply.github.com>
2024-06-09 05:48:47 +0000

    Added the special Methods dunder str and dunder repr

---
+++ OOPS/special_methods.py
@@ -0,0 +1,36 @@
+# by using the Special mthods we will be able to change some of the built-in behaviour and the operations
+#These Special Methods are always surrounded by the Double Underscrore(__)
+# You may heard Dunder init then tey mean __init__
+# These 2 methods (str and repr) are allow us to change how our objects are printed and dispalyed now 
+
+class Employee:
+    raise_amt = 1.04
+    def __init__(self,first,last,pay):
+        self.first = first 
+        self.last = last 
+        self.pay = pay 
+        self.email = first + '.' + last + '@company.com'
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def __repr__(self): # it is an unambigious representation of the object and should be used for the debugging, logging and similar things 
+        return "Employee('{}','{}', '{}' )".format(self.first, self.last, self.pay)
+    
+    def __str__(self): # Meant to be more of a readable representation of an object and to be used as a display to the end user 
+        return '{} - {}'.format(self.fullname(), self.email)
+
+emp1 = Employee('Pankaj','Patil', 500000)
+emp2 = Employee('Akash','Patil', 100000)
+
+#print(emp1)
+#print(repr(emp1))
+#print(str(emp1))
+
+#print(emp1.__repr__())
+#print(emp2.__str__())
+
+# print(1 + 2) # At the backend it uses __add__ Special method to do th addition
+#print(int.__add__(1,2))
+#print(str.__add__('a','b'))

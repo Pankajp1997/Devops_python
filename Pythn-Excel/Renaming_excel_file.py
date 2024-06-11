dc96e3faaca892ef830a95e259cecb22cce34aa1
Pankaj Patil <97191429+Pankajp1997@users.noreply.github.com>
2024-06-11 14:30:38 +0530

    Created Renaming_excel_file.py

---
+++ Pythn-Excel/Renaming_excel_file.py
@@ -0,0 +1,10 @@
+'''
+Why I need this python sciript?
+ans:- We were exporting the logs from an application, The logs are in the format of .xlsx extensions. This files are to be renamed on the value present in the excel file. 
+so, thought of creating the script for that.
+
+What this script does?
+Ans:- It opens all the files present in the folder which having extensions .xlsx. Enters in each file read the particular value, store it and then rename it with the same name. 
+
+Modules Used here are OS, glob and Openpyxl 
+'''
